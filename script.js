95b846b889911ec7b5223b36004bd692f37afe53
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-13 12:31:18 +0600

    files are added

---
+++ script.js
@@ -0,0 +1,14 @@
+let count = 0;
+let totalCount = 0;
+
+// subahanallah counter
+
+document.getElementById('Subhanallah_Counter').onclick = function(){
+    count+=1;
+
+    // will show the total count
+    const totalCount = document.getElementById(count);
+
+    s
+
+}
