7661f50d745f1051dbc7bf987267ca0149cff044
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-13 12:36:34 +0600

    subhanallah done

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let count = 0;
-let totalCount = 0;
+
+
 
 // subahanallah counter
 
 document.getElementById('Subhanallah_Counter').onclick = function(){
     count+=1;
-
+    
     // will show the total count
     const totalCount = document.getElementById(count);
+    // totalCount = count;
+    
+    document.getElementById('counting_items').innerText = count;
+    
+} 
 
-    s
-
-}
