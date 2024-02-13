a5a5897ed0304dc35260628c37ea7393ec2c288b
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-13 14:45:29 +0600

    user input

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const subhanallaBtn = document.getElementById('Subhanallah_Counter');
 
 const subhanallahCountDisplay = document.getElementById('counting_items_sbhn');
 
+const subhanallaResetBtn = document.getElementById('startOver');
+
+// how many times the user want to read
+const inputValue = document.getElementById('inputValueSubhanallah');
+
+
 const alhamdulillahBtn = document.getElementById('alhamdulillah_count');
+
 
 const alhamdulillahCountDisplay = document.getElementById('counting_items_alhm');
 
@@ -15,25 +22,33 @@ let subhanallaCountValue = 0;
 let alhamdulillahCountvalue = 0;
 let allahuakbarCountValue = 0;
 
-let totalCount = subhanallaCountValue+ alhamdulillahCountvalue+ allahuakbarCountValue;
+let totalCount = subhanallaCountValue + alhamdulillahCountvalue + allahuakbarCountValue;
 
-document.getElementById('totalCounter').innerText = totalCount;
+// document.getElementById('totalCounter').innerText = totalCount;
 
-subhanallaBtn.addEventListener('click', function(){
+const totalValue= document.getElementById('')
+subhanallaBtn.addEventListener('click', function () {
     subhanallaCountValue += 1;
     subhanallahCountDisplay.innerText = subhanallaCountValue;
     totalCount.innerText = subhanallaCountValue;
 })
 
-alhamdulillahBtn.addEventListener('click', function(){
+subhanallaResetBtn.addEventListener('click', function () {
+    subhanallaCountValue = 0;
+    subhanallahCountDisplay.innerText = subhanallaCountValue;
+})
+
+
+
+alhamdulillahBtn.addEventListener('click', function () {
     alhamdulillahCountvalue += 1;
     alhamdulillahCountDisplay.innerText = alhamdulillahCountvalue;
     totalCount.innerText = alhamdulillahCountvalue;
 })
 
-allahuakbarBtn.addEventListener('click', function(){
+allahuakbarBtn.addEventListener('click', function () {
     allahuakbarCountValue += 1;
     allahuakbarCountDisplay.innerText = allahuakbarCountValue;
 })
 
-totalCount = subhanallaCountValue+ alhamdulillahCountvalue+ allahuakbarCountValue;
+totalCount = subhanallaCountValue + alhamdulillahCountvalue + allahuakbarCountValue;
